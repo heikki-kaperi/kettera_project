f1cf1df0ceed2806dd1022ae68a68b611da7d74b
Heikki Kaperi <heikki.kaperi@student.lab.fi>
2025-11-26 16:46:59 +0200

    arttuolli3 + kerkko + fixes (sql_query) = 4

---
+++ Kerkko4/ContractManagement.Model.Entities/MyProject.UI/Internal.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace MyProject.UI
+{
+    public partial class Internal : Form
+    {
+        public Internal()
+        {
+            InitializeComponent();
+        }
+
+        private void Internal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // 1. Block & Category Management
+        private void blockCategoryBtn_Click(object sender, EventArgs e)
+        {
+            BlockCategory form = new BlockCategory();
+            form.ShowDialog();
+        }
+
+        // 2. Contract Management
+        private void contractManagementBtn_Click(object sender, EventArgs e)
+        {
+            ContractManagement form = new ContractManagement();
+            form.ShowDialog();
+        }
+
+        // 3. My Contracts (as Reviewer)
+        private void myContractsBtn_Click(object sender, EventArgs e)
+        {
+            MyContracts form = new MyContracts();
+            form.ShowDialog();
+        }
+
+        // 4. View Contract Details
+        private void viewContractDetailsBtn_Click(object sender, EventArgs e)
+        {
+            ContractDetails form = new ContractDetails();
+            form.ShowDialog();
+        }
+
+        // 5. Comment on Contract
+        private void commentContractBtn_Click(object sender, EventArgs e)
+        {
+            CommentContract form = new CommentContract();
+            form.ShowDialog();
+        }
+
+        // 6. Approve Contract
+        private void approveContractBtn_Click(object sender, EventArgs e)
+        {
+            ApproveContract form = new ApproveContract();
+            form.ShowDialog();
+        }
+
+        // 0. Logout → takaisin LogIn.cs
+        private void logoutBtn_Click(object sender, EventArgs e)
+        {
+            this.Hide(); // piilota Internal-näkymä
+            logIn loginForm = new logIn();
+            loginForm.Show();
+        }
+    }
+}
---
+++ Sofia4/ContractManagement.Model.Entities/MyProject.UI/ViewInternalUsers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+using ContractManagement.Controller;
+
+namespace MyProject.UI
+{
+    public partial class ViewInternalUsers : Form
+    {
+        private UserController _userController;
+
+        public ViewInternalUsers(UserController userController)
+        {
+            InitializeComponent();
+            _userController = userController;
+            LoadInternalUsers();
+        }
+
+        private void LoadInternalUsers()
+        {
+            try
+            {
+                var users = _userController.GetAllInternalUsers();
+                dataGridViewUsers.DataSource = users;
+                dataGridViewUsers.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
